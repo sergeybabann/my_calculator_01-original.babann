292d655ca829adb480d2cf6e77326d1219c3718c
sergeybabann <sergeybabann@yandex.ru>
2024-10-13 13:02:54 +0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+document.getElementById("one").addEventListener('click', function() {
+    screen = one.value
+})
+
+document.getElementById("two").addEventListener('click', function() {
+    screen = two.value
+})
+
+document.getElementById("three").addEventListener('click', function() {
+    screen = three.value
+})
+
+document.getElementById("four").addEventListener('click', function() {
+    screen = four.value
+})
+
+document.getElementById("five").addEventListener('click', function() {
+    screen = five.value
+})
+
+document.getElementById("six").addEventListener('click', function() {
+    screen = six.value
+})
+
+document.getElementById("seven").addEventListener('click', function() {
+    screen = seven.value
+})
+
+document.getElementById("eight").addEventListener('click', function() {
+    screen = eight.value
+})
+
+document.getElementById("nine").addEventListener('click', function() {
+    screen = nine.value
+})
+
+document.getElementById("zero").addEventListener('click', function() {
+    screen = zero.value
+})
+
+document.getElementById("clear").addEventListener('click', function() {
+    screen = "clear.value"
+})
+
+document.getElementById("addition").addEventListener('click', function() {
+    screen = "addition.value"
+})
+
+document.getElementById("subtraction").addEventListener('click', function() {
+        screen = "subtraction.value"
+})
+
+document.getElementById("multiplication").addEventListener('click', function() {
+        screen = "multiplication.value"
+})
+
+document.getElementById("division").addEventListener('click', function() {
+    screen = "division.value"
+})
+
+document.getElementById("equal").addEventListener('click', function() {
+    screen = "equal.value"
+})
+
+
+
+
