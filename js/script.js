86cffbcf8a3d5a8eb31f8d427584aa0972489507
sergeybabann <sergeybabann@yandex.ru>
2024-10-13 21:19:28 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,66 +1,69 @@
-document.getElementById("one").addEventListener('click', function() {
-    screen = one.value
-})
+// константа захвата по id поля для ввода символов
+// const display = document.getElementById('display')
+let firstNumber = '' // первое число
+let secondNumber = '' // второе число
+let operation = null // операция, (например, '+')
 
-document.getElementById("two").addEventListener('click', function() {
-    screen = two.value
-})
 
-document.getElementById("three").addEventListener('click', function() {
-    screen = three.value
-})
+// Функция для добавления символа в поле ввода
+function addToDisplay(textToAdd) {
+    // display.value = dispaly.value + textToAdd
+    display.value += textToAdd
+    
+} 
 
-document.getElementById("four").addEventListener('click', function() {
-    screen = four.value
-})
+// Функция для удаления символов из поля ввода
+function clearInput() {
+    display.value = '' // Устанавливаем значение поля ввода в пустую строку
+}
 
-document.getElementById("five").addEventListener('click', function() {
-    screen = five.value
-})
+function plus(result) {
+    display.value = first + second
+}
 
-document.getElementById("six").addEventListener('click', function() {
-    screen = six.value
-})
 
-document.getElementById("seven").addEventListener('click', function() {
-    screen = seven.value
-})
 
-document.getElementById("eight").addEventListener('click', function() {
-    screen = eight.value
-})
+// addToDisplay('1')
 
-document.getElementById("nine").addEventListener('click', function() {
-    screen = nine.value
-})
 
-document.getElementById("zero").addEventListener('click', function() {
-    screen = zero.value
-})
+// Символы цифры
+document.getElementById("one").addEventListener('click', () => addToDisplay('1'))     
 
-document.getElementById("clear").addEventListener('click', function() {
-    screen = "clear.value"
-})
+document.getElementById("two").addEventListener('click', () => addToDisplay('2')) 
 
-document.getElementById("addition").addEventListener('click', function() {
-    screen = "addition.value"
-})
+document.getElementById("three").addEventListener('click', () => addToDisplay('3')) 
 
-document.getElementById("subtraction").addEventListener('click', function() {
-        screen = "subtraction.value"
-})
+document.getElementById("four").addEventListener('click', () => addToDisplay('4')) 
 
-document.getElementById("multiplication").addEventListener('click', function() {
-        screen = "multiplication.value"
-})
+document.getElementById("five").addEventListener('click', () => addToDisplay('5')) 
 
-document.getElementById("division").addEventListener('click', function() {
-    screen = "division.value"
-})
+document.getElementById("six").addEventListener('click', () => addToDisplay('6')) 
 
-document.getElementById("equal").addEventListener('click', function() {
-    screen = "equal.value"
-})
+document.getElementById("seven").addEventListener('click', () => addToDisplay('7')) 
+
+document.getElementById("eight").addEventListener('click', () => addToDisplay('8')) 
+
+document.getElementById("nine").addEventListener('click', () => addToDisplay('9')) 
+
+document.getElementById("zero").addEventListener('click', () => addToDisplay('0')) 
+
+
+// Символы не цифры
+document.getElementById("addition").addEventListener('click', () => addToDisplay('+')) 
+
+document.getElementById("subtraction").addEventListener('click', () => addToDisplay('-')) 
+
+document.getElementById("multiplication").addEventListener('click', () => addToDisplay('*')) 
+
+document.getElementById("division").addEventListener('click', () => addToDisplay('/')) 
+
+document.getElementById("equal").addEventListener('click', () => addToDisplay('=')) 
+
+document.getElementById("clear").addEventListener('click', () => clearInput('')) 
+
+document.getElementById("")
+
+
 
 
 
